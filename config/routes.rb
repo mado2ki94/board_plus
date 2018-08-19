92ad4c4d9e7ed7d84ea759b6b748734f8975b373
吹田洋輔 <suita@mba-2.local>
2018-08-19 19:25:17 +0900

    Add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'mythreads/index'
-  get 'mythreads/new'
-  get 'mythreads/create'
-  get 'mythreads/edit'
-  get 'mythreads/update'
-  get 'mythreads/destroy'
-  get 'mythreads/show'
+  root 'static_pages#index'
   devise_for :users
   resources :mythreads
 end
