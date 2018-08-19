c5335b430703a0aa614aa5ca59cd69643f14e512
吹田洋輔 <suita@mba-2.local>
2018-08-19 19:33:30 +0900

    modify model name in mythread_controller

--- app/controllers/mythreads_controller.rb
+++ app/controllers/mythreads_controller.rb
@@ -1,42 +1,42 @@
 class MythreadsController < ApplicationController
-  before_action :find_mythread, only: [:edit, :update, :destroy, :show]
+  before_action :find_Mythread, only: [:edit, :update, :destroy, :show]
   def index
-    @mythreads = MyThread.all
+    @Mythreads = Mythread.all
   end
 
   def show
   end
 
   def new
-    @mythread = MyThread.new
+    @Mythread = Mythread.new
   end
 
   def create
-    @mythread = MyThread.new(mythread_params)
-    @mythread.save
-    redirect_to @mythread
+    @Mythread = Mythread.new(Mythread_params)
+    @Mythread.save
+    redirect_to @Mythread
   end
 
   def edit
   end
 
   def update
-    @mythread.update_attributes(mythread_params)
-    redirect_to @mythread
+    @Mythread.update_attributes(Mythread_params)
+    redirect_to @Mythread
   end
 
   def destroy
-    @mythread.destroy
-    redirect_to @mythread
+    @Mythread.destroy
+    redirect_to @Mythread
   end
 
   private
 
-    def find_mythread
-      @mythread = MyThread.find(params["id"])
+    def find_Mythread
+      @Mythread = Mythread.find(params["id"])
     end
 
-    def mythread_params
-      params.require(:mythread).permit(:title, :comment)
+    def Mythread_params
+      params.require(:Mythread).permit(:title, :comment)
     end
 end
